041b8fdba38f980782cd6ca2f517da8d0c165074
Liam <liamnplenza@gmail.com>
2022-04-27 11:22:43 -0300

    Creo menu reportes

--- trabajo_practico_copia.py
+++ trabajo_practico_copia.py
@@ -1,4 +1,3 @@
-from asyncio.windows_events import NULL
 import os, time
 
 def menu_principal():
@@ -35,106 +34,64 @@ def menu_opciones():
         time.sleep(2)
         menu_opciones()
 
-    #titulares = [str]
-    #my_dict_funciones = {
-    #    1: alta,
-    #    2: baja,
-    #    3: consultar,
-    #    4: modificacion,
-    #    0: volver_al_menu
-    #}
-    
-#def alta(titulares):
-#    titulares[c]= input("Ingrese el nombre del titular" )
-#    c= c+1
-#    #poner lo del enter para continuar
-#    menu_opciones()
-
-#def baja(titulares):
-#        titularF = input("Ingrese el nombre del titular a eliminar")
-#        co = 0
-#        while co < c:
-#            if titulares[c] == titularF:
-#                pass
-#            else:
-#                co= co +1
-        
-#def consultar(titulares):
-#    titularF = input("Ingrese el nombre del titular a consultar")
-#    co = 0
-#    while co < c:
-#        if titulares[c] == titularF:
-#            print("El titular se encuentra en el sistema")
-#            menu_opciones()
-#        else:
-#            co= co + 1
-#            if co==c:
-#                print("El titular no se encuentra en el sistema")
-#                menu_opciones()
-
-#def modificacion(titulares):
-#    pass
-
-def menu_recepcion():
+def menu_recepcion(camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja):
     clear_shell()
     print("0 - Volver al menu anterior\n1 - Ingresar un nuevo camion")
     
     option = int(input("Seleccione una opción del menu: "))
     while option != 0:
         if option == 1:
-            ingreso_de_datos()
-        elif option == 74:
-            print(" grrr Felicidades! Encontró un easter egg, vamos la lepra")
+            camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja = ingreso_de_datos(camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja)
         else:
             print("Ingrese una opción valida del menu... Imbecil")
             time.sleep(2) 
         menu_recepcion()
-
-
-pesoMayorSoja = 0
-pesoMenorMaiz = 0
-
-def ingreso_de_datos():
-    tipoCamion = input ("Ingrese si el camion contiene Soja o Maíz").upper()
-    tara = float(input ("Ingrese la tara del camión"))
-    pesoBruto = float(input ("Ingrese el peso bruto del camión"))
+    return camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja
+    
+def ingreso_de_datos(camionMaiz, pesoNetoMai, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja): 
+    tipoCamion = input("Ingrese si el camion contiene Soja o Maíz: ").upper()
     patCamion = input("Ingrese la patente: ").upper()
-    pesoNeto = pesoBruto - tara
-    print("El peso neto del camión ingresado es",pesoNeto)
+    pesoNeto = float(input("Ingrese el peso bruto del camión: ")) - float(input("Ingrese la tara del camión: "))
+    
+    #CAnt camion = camionsoja+ camion maiz
+    print("El peso neto del camión ingresado es: ",pesoNeto)
     time.sleep(2)
+
     if tipoCamion == "SOJA":
-        camionSoja= camionSoja + 1
+        camionSoja= camionSoja + 1 
         pesoNetoSoja = pesoNetoSoja + pesoNeto
-        promPesoNetoS = pesoNetoSoja / camionSoja
+        #promPesoNetoS = pesoNetoSoja / camionSoja
         if pesoNetoSoja > pesoMayorSoja:
             pesoMayorSoja = pesoNetoSoja
             patMayorSoja = patCamion
     elif tipoCamion == "MAIZ":
         camionMaiz = camionMaiz + 1
         pesoNetoMaiz = pesoNetoSoja + pesoNeto
-        promPesoNetoM = pesoNetoMaiz / camionMaiz
+        #promPesoNetoM = pesoNetoMaiz / camionMaiz
         if pesoNetoMaiz < pesoMenorMaiz:
             pesoMenorMaiz = pesoNetoMaiz
             patMenorMaiz = patCamion
     else: 
         print("Ingrese un Proucto valido")
         ingreso_de_datos()
-    cantCamion= cantCamion + 1
+    return camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja
 
-def menu_reportes():
+def menu_reportes(camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja):
     print("0 - Volver al menu anterior\n1 - Msotrar el reporte actual")
     
     option = int(input("Seleccione una opción del menu: "))
     if option == 0:
         menu_principal()
     elif option == 1:
-        mostrar_reporte()
+        mostrar_reporte(camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja)
     else:
         print("Ingrese una opcion válida")
         clear_shell()
         menu_reportes()
-def mostrar_reporte():
-    pass
+
+def mostrar_reporte(camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja):
+    return print(f"La cantidad total de camiones es: {camionMaiz + camionSoja}\nLa cantidad de camiones de maiz es: {camionMaiz}\nLa cantidad de camiones de soja es: {camionSoja}\nEl peso neto correspondiente al maiz es: {pesoNetoMaiz}\nEl peso neto correspondiente a la soja es: {pesoNetoSoja}\nEl promedio del peso neto correspondiente al maíz es: {pesoNetoMaiz/camionMaiz}\nEl promedio del peso neto correspondiente a la soja es: {pesoNetoSoja/camionSoja}\nLa patente correspondiente al camión que menos maíz descargo es: {patMenorMaiz}\nLa patente correspondiente al camión que más soja descargo es: {patMayorSoja}")
+
 # para determinar el sistema operativo y limpiar la consola
 def clear_shell():
     if os.name ==  "nt":
@@ -142,31 +99,31 @@ def clear_shell():
     else:
         return os.system("clear")
 
-#vamoslalepra
 if __name__ == "__main__":
+    camionMaiz = pesoNetoMaiz = pesoMenorMaiz = camionSoja = pesoNetoSoja = pesoMayorSoja = 0
+    patMayorSoja = patMenorMaiz = ""
     menu_principal()
     option = int(input("Seleccione una opción del menu: "))
     
     #diccionario con las opciones y las funciones correspondientes
-    my_dict = {
-        1: menu_administraciones,
-        3: menu_recepcion,
-        8: menu_reportes
-    }
+    #my_dict = {
+    #    1: menu_administraciones,
+    #    3: menu_recepcion,
+    #    8: menu_reportes
+    #}
 
     while option != 0:
         #busca la llave correspondiente a la opcion, si existe llama a la funcion sino ejecuta lambda -> reemplaza los if
-        my_dict.get(option, lambda: print("Esta funcionalidad está en construcción"))()
+        #my_dict.get(option, lambda: print("Esta funcionalidad está en construcción"))()
 
-        #if option == 1:
-        #    menu_administraciones()
-        #elif option == 3:
-        #    menu_recepcion()
-        #elif option == 8:
-        #    menu_reportes
-        #else:
-        # 3
-        # 3   print("Esta funcionalidad está en construcción \n")
+        if option == 1:
+            menu_administraciones()
+        elif option == 3:
+            menu_recepcion(camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja)
+        elif option == 8:
+            menu_reportes(camionMaiz, pesoNetoMaiz, patMenorMaiz, pesoMenorMaiz, camionSoja, pesoNetoSoja, patMayorSoja, pesoMayorSoja)
+        else:
+          print("Esta funcionalidad está en construcción \n")
         
         menu_principal()
         option = int(input("Seleccione una opción del menu: "))
